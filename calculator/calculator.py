c82a2bb7c0098957fb1dfd7f45f7e9d540cd1bbc
Scott <sgeter@gmail.com>
2023-01-24 19:53:20 -0500

    add test-results, add division to calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -8,3 +8,6 @@ def multiply(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+def divide(first_term, second_term):
+    return first_term/second_term
